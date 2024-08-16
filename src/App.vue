c2b1f8d05e853d0f78bb17d6ac830f3481cda1cd
manyukV <manyukwork@mail.ru>
2024-08-16 19:25:18 +0300

    header done

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import Header from './components/header.vue'
 </script>
 
 <template>
-  <Header></Header>
+  <div class="bg-white h-screen w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <Header />
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,5 +1,34 @@
-<script setup></script>
+<script setup>
+defineProps({
+  title: String,
+  price: Number
+})
+</script>
 
-<template></template>
+<template>
+  <header class="flex justify-between border-b px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="logo" class="w-10" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Sneakers</h2>
+        <p class="text-slate-500">Магазин крососвок</p>
+      </div>
+    </div>
+    <ul class="flex justify-between gap-10">
+      <li class="flex items-center gap-3 text-gray-500 cursor-pointer hover:text-black">
+        <img src="/cart.svg" alt="cart" />
+        <span>1205 руб</span>
+      </li>
+      <li class="flex items-center gap-3 text-gray-500 hover:text-black">
+        <img src="/heart.svg" alt="cart" />
+        <span>Закладки</span>
+      </li>
+      <li class="flex items-center gap-3 text-gray-500 hover:text-black">
+        <img src="/profile.svg" alt="cart" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
 
 <style scoped></style>
