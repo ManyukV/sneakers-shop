59053ce0ffbf554b98bfc19a200fab6cf65cd9c5
manyukV <manyukwork@mail.ru>
2024-08-17 17:39:14 +0300

    options and search

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,34 @@
 import Header from './components/header.vue'
 import cardList from './components/card-list.vue'
 import Banner from './components/banner.vue'
-import aboutUs from './components/about-us.vue'
 </script>
 
 <template>
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
     <Banner />
+
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none">
+            <option>По названию</option>
+            <option>По цене(по убыванию)</option>
+            <option>По цене(по возрастанию)</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute left-3 top-3" src="/search.svg" alt="search" />
+            <input
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              type="text"
+              placeholder="Поиск"
+            />
+          </div>
+        </div>
+      </div>
       <cardList />
     </div>
   </div>
