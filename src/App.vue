9297b417d6808daf5a916f6bf6c82b93e4501413
manyukV <manyukwork@mail.ru>
2024-08-17 17:23:20 +0300

    banner

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Header from './components/header.vue'
 import cardList from './components/card-list.vue'
-import modalBusket from './components/modal-busket.vue'
+import Banner from './components/banner.vue'
+import aboutUs from './components/about-us.vue'
 </script>
 
 <template>
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
+    <Banner />
     <div class="p-10">
       <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
       <cardList />
---
+++ src/components/banner.vue
@@ -0,0 +1,9 @@
+<script></script>
+
+<template>
+  <div class="relative">
+    <img class="w-full h-full" src="/banner.jpg" alt="" />
+  </div>
+</template>
+
+<style></style>
