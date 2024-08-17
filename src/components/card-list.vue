c120a41e78fc8c3992f033df6830427238f04689
manyukV <manyukwork@mail.ru>
2024-08-17 16:24:45 +0300

    card and cardList done

--- src/components/card-list.vue
+++ src/components/card-list.vue
@@ -1,13 +1,25 @@
 <script setup>
 import Card from './card.vue'
+
+function onClickAdd() {
+  alert('Добавленно в корзину')
+}
+
+function onClickFavor() {
+  alert('Добавленно в избранное')
+}
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card
+      img-url="/sneakers/sneakers-1.jpg"
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      :price="1200"
+      :is-add="false"
+      :is-favor="false"
+      :on-click-add="onClickAdd"
+      :on-click-favor="onClickFavor"
+    />
   </div>
 </template>
--- src/components/card.vue
+++ src/components/card.vue
@@ -1,16 +1,33 @@
+<script setup>
+defineProps({
+  imgUrl: String,
+  title: String,
+  price: Number,
+  isFavor: Boolean,
+  isAdd: Boolean,
+  onClickAdd: Function,
+  onClickFavor: Function
+})
+</script>
+
 <template>
   <div
     class="relative border bg-white border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition"
   >
-    <img class="absolute top-8 left-8" src="/like-1.svg" alt="like" />
-    <img src="/sneakers/sneakers-1.jpg" alt="sneaker" />
-    <p class="mt-2">Мужские Кроссовки Nike Blazer Mid Suede</p>
+    <img
+      @click="onClickFavor"
+      class="absolute top-8 left-8"
+      :src="!isFavor ? '/like-1.svg' : '/like-2.svg'"
+      alt="like"
+    />
+    <img :src="imgUrl" alt="sneaker" />
+    <p class="mt-2">{{ title }}</p>
     <div class="flex justify-between mt-5">
       <div class="flex flex-col">
         <span class="text-slate-400">Цена:</span>
-        <b>1200p</b>
+        <b>{{ price }} BYN</b>
       </div>
-      <img src="/plus.svg" alt="plus" />
+      <img @click="onClickAdd" :src="!isAdd ? '/plus.svg' : '/checked.svg'" alt="plus" />
     </div>
   </div>
 </template>
